8909a915a6bc47f6c187099e1c11523426ee7eb7
sqliu <sqliu@thoughtworks.com>
2013-12-11 13:24:27 +0800

    Enable line number.

--- personal/custom.el
+++ personal/custom.el
@@ -4,3 +4,8 @@
 ;; swap meta and super keys on OS X
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
+
+;; enable line number
+(global-linum-mode t)
+(setq linum-format " %d ")
+(global-set-key (kbd "M-l") 'goto-line)
